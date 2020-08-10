d5a61c45dc8aad0208c9fc46278e36af573969cc
Pavel <pasha.krupko@yandex.ru>
2020-08-10 22:37:37 +0300

    Доработка сеток для всех версий страниц

--- source/js/navigation.js
+++ source/js/navigation.js
@@ -1,6 +1,6 @@
 var burgerToggle = document.querySelector(".burger");
 
 burgerToggle.addEventListener("click", function (evt) {
-    burgerToggle.classList.toggle("burger--opened");
-    burgerToggle.classList.toggle("burger--closed");
+  burgerToggle.classList.toggle("burger--opened");
+  burgerToggle.classList.toggle("burger--closed");
 });
