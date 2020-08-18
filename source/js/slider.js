95bd335e1b6246141ab78698f7765668b4f23985
Pavel <pasha.krupko@yandex.ru>
2020-08-18 17:29:13 +0300

    Доработка ретиновой графики

--- source/js/slider.js
+++ source/js/slider.js
@@ -2,11 +2,13 @@ var sliderBtnBefore = document.querySelector(".slider-controls__btn--before");
 var sliderBtnAfter = document.querySelector(".slider-controls__btn--after");
 var slideBefore = document.querySelector(".example__slide--before");
 var slideAfter = document.querySelector(".example__slide--after");
+var sliderBar = document.querySelector(".slider-controls__bar");
 
 sliderBtnBefore.addEventListener("click", function (evt) {
   if (!slideBefore.classList.contains("example__slide--current")) {
     slideBefore.classList.add("example__slide--current");
     slideAfter.classList.remove("example__slide--current");
+    sliderBar.classList.remove("slider-controls__bar--after");
   }
 });
 
@@ -14,5 +16,6 @@ sliderBtnAfter.addEventListener("click", function (evt) {
   if (!slideAfter.classList.contains("example__slide--current")) {
     slideAfter.classList.add("example__slide--current");
     slideBefore.classList.remove("example__slide--current");
+    sliderBar.classList.add("slider-controls__bar--after");
   }
 });
